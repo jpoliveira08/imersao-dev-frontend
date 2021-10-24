6f865ebf44211779bb1eeba98f9ff66daaec46df
jpoliveira08 <joaopedro.oliveirajava@gmail.com>
2021-10-24 11:36:49 -0300

    Alterações no Day04

--- Day04/index.php
+++ Day04/index.php
@@ -1,6 +1,5 @@
 <?php
     require_once './src/Model/Iteracao.php';
-    session_start();
 
 ?>
 <!DOCTYPE html>
@@ -10,6 +9,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="/src/css/main.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
     <title>Aluraflix</title>
 </head>
 <body>
@@ -17,17 +21,37 @@
         <h1 class="page-title">Aluraflix</h1>
         <img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/logo-imersao-aluraflix.svg" class="page-logo" alt="">
     </div>
-
-    <button class="voltar">Anterior</button>
-    <a href="https://www.imdb.com/title/tt0241527/" target="_brank"><img class="imagem" src="/img/filme1.jpg"></a>
-    <button class="avancar">Próximo</button>
-
-    <p>
-        <?php
-
-        ?>
-    </p>
-
+    <div class="carrosel">
+        <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+            <div class="carousel-inner">
+                <?php
+                    $controle_ativo = 2;
+                    $iteracao = new Iteracao();
+                    $filmes = $iteracao->getFilmes();
+                    foreach ($filmes as $chave => $valor) {
+                        if($controle_ativo == 2) : ?>
+                        <div class="carousel-item active">
+                            <img class="imagem" src="<?php echo $valor?>" alt="">
+                            <h3 class="page-subtitle text-monospace"><?php echo $chave?></h3>
+                        </div>
+                        <?php $controle_ativo = 1;
+                        else :?>
+                            <div class="carousel-item">
+                                <img class="imagem" src="<?php echo $valor ?>" alt="">
+                                <h3 class="page-subtitle text-monospace"><?php echo $chave?></h3>
+                            </div>
+                        <?php endif;}?>
+            </div>
+            <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                <span class="sr-only">Previous</span>
+            </a>
+            <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                <span class="sr-only">Next</span>
+            </a>
+        </div>
+    </div>
 
 </body>
 </html>
--- Day04/src/Model/Iteracao.php
+++ Day04/src/Model/Iteracao.php
@@ -4,22 +4,19 @@
 class Iteracao
 {
     private $arrayFilmes = [
-        'Harry Potter e a Pedra Filosofal',
-        'Harry Potter e a Câmara Secreta',
-        'Harry Potter e o Prisioneiro de Azkaban',
-        'Harry Potter e o Cálice de Fogo',
-        'Harry Potter e a Ordem da Fênix',
-        'Harry Potter e o Enigma do Príncipe',
-        'Harry Potter e as Relíquias da Morte: Parte 1',
-        'Harry Potter e as Relíquias da Morte: Parte 2'
+        'Harry Potter e a Pedra Filosofal'=>'/img/filme1.jpg',
+        'Harry Potter e a Câmara Secreta'=>'/img/filme2.jpg',
+        'Harry Potter e o Prisioneiro de Azkaban'=>'/img/filme3.jpg',
+        'Harry Potter e o Cálice de Fogo'=>'/img/filme4.jpg',
+        'Harry Potter e a Ordem da Fênix'=>'/img/filme5.jpg',
+        'Harry Potter e o Enigma do Príncipe'=>'/img/filme6.jpg',
+        'Harry Potter e as Relíquias da Morte: Parte 1'=>'/img/filme7.jpg',
+        'Harry Potter e as Relíquias da Morte: Parte 2'=>'/img/filme8.png'
     ];
 
     public function getFilmes() : array
     {
         return $this->arrayFilmes;
     }
-    public function setFilme(string $filme) : void
-    {
-        array_push($arrayFilmes, $filme);
-    }
+
 }
